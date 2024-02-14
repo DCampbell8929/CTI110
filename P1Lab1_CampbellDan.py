f412e1ef5afd9bb5c8436c3ddb117f575655b751
DCampbell8929 <160029375+DCampbell8929@users.noreply.github.com>
2024-02-14 08:26:18 -0500

    P1 Initial Submission
    P1 Initial Submission

---
+++ P1Lab1_CampbellDan.py
@@ -0,0 +1,14 @@
+#Dan Campbell
+#2/12/2024
+#P1Lab1
+#Request name from user->
+#Assign input to variable(name)->
+#Print a welcome message to the user->
+#End
+
+#Creating a variable to hold first name
+first_name = input("Please enter your first name: ")
+
+#Print first_name with greeting.
+print("Hey",first_name)
+print("Welcome to zyBooks")
